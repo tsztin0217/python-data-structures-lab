95f9583842cc54e9b203b7e1375ae51b6dd606ee
tsztin0217 <tsztin0217@gmail.com>
2025-04-15 22:41:39 -0400

    Complete Exercise 2

--- exercises.py
+++ exercises.py
@@ -30,3 +30,20 @@ def manage_students():
 
 # Call the function and print the result
 print('Exercise 1:', manage_students())
+
+
+# Exercise 2: Loop and String Concatenation
+#
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Create a variable named meal and assign an empty string to it.
+# Use a for loop to iterate over the strings in foods and append each string to meal.
+
+def combine_foods():
+    foods = ('tofu', 'bread', 'banana')
+    meal = ''
+    for food in foods:
+        meal += food + ' '
+    return meal 
+
+# Call the function and print the result
+print('Exercise 2:', combine_foods())
