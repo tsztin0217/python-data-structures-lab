3154439520727bb1101e080fa1bc2e3e235e9cd4
tsztin0217 <tsztin0217@gmail.com>
2025-04-15 23:16:44 -0400

    Complete Exercise 5

--- exercises.py
+++ exercises.py
@@ -47,3 +47,54 @@ def combine_foods():
 
 # Call the function and print the result
 print('Exercise 2:', combine_foods())
+
+
+# Exercise 3: Slicing Tuples
+#
+# Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
+
+def slice_foods():
+    foods = ('tofu', 'bread', 'banana')
+    last_two_foods = foods[-2:]
+    return last_two_foods
+
+# Call the function and print the result
+print('Exercise 3:', slice_foods())
+
+
+# Exercise 4: Dictionaries and String Formatting
+#
+# Create a dictionary named home_town containing the keys of city, state, and population.
+# Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
+
+def hometown_info():
+    home_town = {
+        'city': 'Novato',
+        'state': 'California',
+        'population': '1000'
+    }
+    home_town_message = f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
+    return home_town_message
+
+# Call the function and print the result
+print('Exercise 4:', hometown_info())
+
+
+# Exercise 5: Iterating Over Dictionary Items
+#
+# Define an empty list named home_town_items.
+# Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
+
+def list_home_town_items():
+    home_town_items = []
+    home_town = {
+        'city': 'Novato',
+        'state': 'California',
+        'population': '1000'
+    }
+    for key, val in home_town.items():
+        home_town_items.append(f"{key} = {val}")
+    return home_town_items
+
+# Call the function and print the result
+print('Exercise 5:', list_home_town_items())
